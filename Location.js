18a7b257e931f78f8f7c03063b984e9aa81a65a5
Rami Alhussein <Alhusseinr1@montclair.edu>
2018-09-15 07:55:49 -0400

    making small changes to Location.js

--- Location.js
+++ Location.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Platform, Text, View, StyleSheet } from 'react-native';
 import { Constants, Location, Permissions } from 'expo';
 
-export default class App extends Component {
+export default class Location extends Component {
   state = {
     location: null,
     errorMessage: null,
