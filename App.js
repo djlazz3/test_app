94a845c79f9b3b0724d9561cc3745d6c04287553
tisla005 <tisla005@fiu.edu>
2018-09-15 07:55:49 -0400

    Camera, photo, and gallery components
    Components for the camera functionality

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
+import { Constants, Camera, FileSystem, Permissions, BarCodeScanner } from 'expo';
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 import StuffComponent from './StuffComponent'
 import StatelessFunctionalComponent from './StatelessFunctionalComponent'
+import CameraComponent from './CameraComponent'
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
+        <CameraComponent />
         <StuffComponent visible />
         <StatelessFunctionalComponent stuff="dank memes"/>
       </View>
